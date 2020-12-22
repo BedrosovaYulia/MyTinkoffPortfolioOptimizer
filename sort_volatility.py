0d63ddb25a169e1d9267017ae24b9a270d855e77
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-12-22 18:23:27 +0300

    some impruve

--- sort_volatility.py
+++ sort_volatility.py
@@ -31,11 +31,12 @@ def main():
     df = dict()
     k = 0
     for MI in markets.instruments:
+        print ("Analizing "+MI.ticker)
         now = datetime.now()
         try:
             cndls = api.market.market_candles_get(MI.figi,
                                                     from_=now -
-                                                    timedelta(days=7),
+                                                    timedelta(days=250),
                                                     to=now,
                                                     interval=ti.CandleResolution.day)
             df2 = dict()
@@ -49,15 +50,14 @@ def main():
     pddf = pd.DataFrame(df)
     pddf.index = pd.to_datetime(pddf.index).to_period('d')
 
-    valotil = erk.annualize_vol(pddf, 7)
-    a_rets = erk.annualize_rets(pddf, 7)
-    sharp_r = erk.sharpe_ratio(pddf, 0.1, 7)
+    # 250 because 250 trading days a year
+    valotil = erk.annualize_vol(pddf, 250)
+    a_rets = erk.annualize_rets(pddf, 250)
+    sharp_r = erk.sharpe_ratio(pddf, 0.1, 250)
 
     
     result = pd.DataFrame(dict(vol=valotil, rets=a_rets, sharp=sharp_r)).reset_index()
     print(result.head())
-
-    #Список самых волатильных акций - сохранить в CSV
 
     result.to_csv("volatility2.csv")
    
