72981f050a021adf00e3ec7ac95ba6ebd586dd85
BedrosovaYulia <BedrosovaYulia@gmail.com>
2021-01-01 19:02:49 +0300

    Update tinkoff_stdata.py

--- tinkoff_stdata.py
+++ tinkoff_stdata.py
@@ -4,33 +4,30 @@ import asyncio
 from openapi_client.openapi_streaming import run_stream_consumer
 from openapi_client.openapi_streaming import print_event
 
+token = os.getenv('TINVEST_SANDBOX_TOKEN', '')
 
 
-"""async def main() -> None:
-    async with ti.Streaming(config.TOKEN) as streaming:
-        await streaming.candle.subscribe('BBG0013HGFT4', ti.CandleResolution.min1)
-        await streaming.orderbook.subscribe('BBG0013HGFT4', 5)
-        await streaming.instrument_info.subscribe('BBG0013HGFT4')
-        async for event in streaming:
-            print(event)  # noqa:T001
+async def main() -> None:
+
+        candle_subs = [{'figi': 'BBG000B9XRY4', 'interval': '1min'},
+               {'figi': 'BBG009S39JX6', 'interval': '1min'}]
+        orderbook_subs = [{'figi': 'BBG0013HGFT4', 'depth': 1},
+                        {'figi': 'BBG009S39JX6', 'depth': 3}]
+        instrument_info_subs = [{'figi': 'BBG000B9XRY4'}, {'figi': 'BBG009S39JX6'}]
+
+        await run_stream_consumer(token,
+                            candle_subs, orderbook_subs, instrument_info_subs,
+                            on_candle_event=print_event,
+                            on_orderbook_event=print_event,
+                            on_instrument_info_event=print_event)
 
 
 try:
     asyncio.run(main())
 except KeyboardInterrupt:
-    pass"""
+    pass
 
 
-token = os.getenv('TINVEST_SANDBOX_TOKEN', '')
+"""
 
-candle_subs = [{'figi': 'BBG000B9XRY4', 'interval': '1min'},
-               {'figi': 'BBG009S39JX6', 'interval': '1min'}]
-orderbook_subs = [{'figi': 'BBG0013HGFT4', 'depth': 1},
-                  {'figi': 'BBG009S39JX6', 'depth': 3}]
-instrument_info_subs = [{'figi': 'BBG000B9XRY4'}, {'figi': 'BBG009S39JX6'}]
-
-run_stream_consumer(token,
-                    candle_subs, orderbook_subs, instrument_info_subs,
-                    on_candle_event=print_event,
-                    on_orderbook_event=print_event,
-                    on_instrument_info_event=print_event)
+"""
