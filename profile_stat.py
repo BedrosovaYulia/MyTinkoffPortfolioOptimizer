23c3ba189d8d6397872d53b048f0643b311a4f91
BedrosovaYulia <BedrosovaYulia@gmail.com>
2021-01-03 09:44:01 +0300

    Update profile_stat.py

--- profile_stat.py
+++ profile_stat.py
@@ -14,23 +14,44 @@ def print_30d_operations():
     yesterday = now - timedelta(days=30)
     ops = client.operations.operations_get(_from=yesterday.isoformat(), to=now.isoformat())
     return ops
+
+def get_portfilio():
+    prt=client.portfolio.portfolio_get()
+    return prt
+
+def get_market_names():
+    names = client.market.market_stocks_get()
+    return names
+
+figi_name=dict()
+name_figi=dict()
+instruments = get_market_names()
+#print(instruments.payload.instruments)
+for instrument in instruments.payload.instruments:
+    figi_name[instrument.figi]=instrument.name
+    name_figi[instrument.name] = instrument.figi
+
+#print(figi_name)
     
+figi_in_prt=list()
+prt=get_portfilio()
+for pos in prt.payload.positions:
+    figi_in_prt.append(pos.figi)
 
 operations=print_30d_operations()
 
-#print(operations)
 byfigi=dict()
 for operation in operations.payload.operations:
-    if operation.status == 'Done':
-        byfigi[operation.figi] = {"summ": 0, "currency": operation.currency}
-
-for operation in operations.payload.operations:
-    if operation.status == 'Done':
+    if operation.figi and operation.status == 'Done' and (operation.figi not in figi_in_prt):
+        byfigi[operation.figi] = {
+            "summ": 0, "currency": operation.currency, "name": figi_name[operation.figi]}
+        
+for  operation in operations.payload.operations:
+    if operation.figi and operation.status == 'Done' and (operation.figi not in figi_in_prt):
         byfigi[operation.figi]["summ"] = byfigi[operation.figi]["summ"] + operation.payment
         
         
-#print(operation.date, operation.figi, operation.payment, operation.currency, operation.operation_type)
-
 pddf = pd.DataFrame(byfigi)
 
 print(pddf)
+pddf.to_csv("income.csv")
