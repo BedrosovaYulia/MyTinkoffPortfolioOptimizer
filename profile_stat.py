b9df0416bcc51990b5490a76ccfe308cb675a849
BedrosovaYulia <BedrosovaYulia@gmail.com>
2021-01-03 19:45:58 +0300

    Update profile_stat.py

--- profile_stat.py
+++ profile_stat.py
@@ -23,6 +23,7 @@ def get_market_names():
     names = client.market.market_stocks_get()
     return names
 
+
 figi_name=dict()
 name_figi=dict()
 instruments = get_market_names()
@@ -31,41 +32,58 @@ for instrument in instruments.payload.instruments:
     figi_name[instrument.figi]=instrument.name
     name_figi[instrument.name] = instrument.figi
 
-#print(figi_name)
+print(figi_name)
     
 figi_in_prt=list()
+figi_cost=dict()
 prt=get_portfilio()
 for pos in prt.payload.positions:
+    #print(pos.name, pos.balance*pos.average_position_price.value)
     figi_in_prt.append(pos.figi)
+    figi_cost[pos.figi] = pos.balance*pos.average_position_price.value
 
 operations=print_30d_operations()
 
 byfigi=dict()
+byfigiusd = dict()
 for operation in operations.payload.operations:
-    if operation.figi and operation.status == 'Done' and (operation.figi not in figi_in_prt):
-        byfigi[operation.figi] = {
-            "summ": 0, "currency": operation.currency, "rsumm":0, "name": figi_name[operation.figi]}
-        
-for  operation in operations.payload.operations:
-    if operation.figi and operation.status == 'Done' and (operation.figi not in figi_in_prt):
-        
-        rsumm = operation.payment
-        
-        if operation.currency=="USD":
-            rsumm = operation.payment*70
-        print("*************************************************")
-        print(figi_name[operation.figi], byfigi[operation.figi]
-              ["summ"], "+", operation.payment, "=")
-            
-        byfigi[operation.figi]["summ"] = byfigi[operation.figi]["summ"] + operation.payment
-        byfigi[operation.figi]["rsumm"] = byfigi[operation.figi]["rsumm"] + rsumm
+    if operation.figi and operation.status == 'Done':
+        startsum=0
+        try:
+            startsum = figi_cost[operation.figi]
+        except:
+            pass
+        fgname = ""
+        try:
+            fgname = figi_name[operation.figi]
+        except:
+            pass
+        if operation.currency =="RUB":
+            byfigi[operation.figi] = {
+                "summ": startsum, "currency": operation.currency, "name": fgname}
+        elif operation.currency == "USD":
+            byfigiusd[operation.figi] = {
+                "summ": startsum, "currency": operation.currency, "name": fgname}
 
-        print(figi_name[operation.figi], byfigi[operation.figi]["summ"])
-        print("****************************************************")
+            
+for  operation in operations.payload.operations:
+    if operation.figi and operation.status == 'Done':
+        if operation.currency == "RUB":   
+            byfigi[operation.figi]["summ"] = byfigi[operation.figi]["summ"] + operation.payment
+        elif operation.currency == "USD":
+            byfigiusd[operation.figi]["summ"] = byfigiusd[operation.figi]["summ"] + operation.payment
+        
         
 pddf = pd.DataFrame(byfigi)
 pddf2=pddf.T
 
+pddfusd = pd.DataFrame(byfigiusd)
+pddf2usd = pddfusd.T
+
 print(pddf2)
-print(pddf2["rsumm"].sum())
+print(pddf2["summ"].sum())
 pddf2.to_csv("income.csv")
+
+print(pddf2usd)
+print(pddf2usd["summ"].sum())
+pddf2usd.to_csv("incomeusd.csv")
