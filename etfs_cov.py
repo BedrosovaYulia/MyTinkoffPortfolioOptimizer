92e4ec455eb2afb574860eaf9e75dfa9d47ce2af
BedrosovaYulia <BedrosovaYulia@gmail.com>
2021-01-10 10:34:55 +0300

    Update etfs_cov.py

--- etfs_cov.py
+++ etfs_cov.py
@@ -8,10 +8,14 @@ import csv
 l=[]
 pddf = td.getTinkoffETFsLYPrices(l)
 pddf.index = pd.to_datetime(pddf.index).to_period('d')
+pd.set_option("display.float_format", "{:.2f}".format)
 
 cov = pddf.cov()
 print(cov)
+cov.to_csv("etfs_cov.csv")
 
+print(cov.idxmin())
+cov.idxmin().to_csv("etfs_cov_min.csv")
 
 #returns = pddf.pct_change()[1:]
 #print(returns)
