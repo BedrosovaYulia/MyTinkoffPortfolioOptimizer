9f9444f1bb90735062a415b1769e990944d022dd
BedrosovaYulia <BedrosovaYulia@gmail.com>
2021-01-11 15:10:08 +0300

    cov

--- cov.py
+++ cov.py
@@ -6,15 +6,17 @@ import edhec_risk_kit as erk
 import csv
 
 l=[]
+e = ["SRPT", "TSLA"]
 
-pddf = td.getTinkoffUSDLYPrices(l)
+pddf = td.getTinkoffLastYearPrices(l)
+print(pddf.head())
 pddf.index = pd.to_datetime(pddf.index).to_period('d')
 pd.set_option("display.float_format", "{:.2f}".format)
 
 cov = pddf.cov()
 #print(cov)
-cov.to_csv("cov_usd.csv")
+cov.to_csv("cov.csv")
 
 print(cov.idxmin().head())
 #print(cov.min())
-cov.idxmin().to_csv("cov_usd_min.csv")
+cov.idxmin().to_csv("cov_min.csv")
--- tinkoff_data.py
+++ tinkoff_data.py
@@ -55,71 +55,6 @@ def getTinkoffLastYearPrices(l=[], resolution='day'):
     return pddf
 
 
-def getTinkoffLastYearPrices(l=[], resolution='day'):
-    client = CustomClient(
-        os.getenv('TINVEST_SANDBOX_TOKEN', ''), use_sandbox=True)
-    api = ti.OpenApi(client)
-
-    markets = api.market.market_stocks_get()
-    df = dict()
-    k = 0
-    for MI in markets.instruments:
-        if (len(l) > 0 and MI.ticker in l) or (len(l) == 0 and MI.ticker):
-            now = datetime.now()
-            try:
-                if resolution == 'day':
-                    cndls = api.market.market_candles_get(MI.figi,
-                                                          from_=now -
-                                                          timedelta(days=7),
-                                                          to=now,
-                                                          interval=ti.CandleResolution.day)
-                elif resolution == 'month':
-                    cndls = api.market.market_candles_get(MI.figi,
-                                                          from_=now -
-                                                          timedelta(days=3650),
-                                                          to=now,
-                                                          interval=ti.CandleResolution.month)
-
-                df2 = dict()
-                for cndl in cndls.candles:
-                    df2[str(cndl.time)] = cndl.c
-                df[MI.ticker] = df2
-            except:
-                pass
-            k = k+1
-    pddf = pd.DataFrame(df)
-    return pddf
-
-
-def getTinkoffUSDLYPrices(l=[], f=datetime.now()-timedelta(days=7), t=datetime.now()):
-    print(f,t)
-    client = CustomClient(
-        os.getenv('TINVEST_SANDBOX_TOKEN', ''), use_sandbox=True)
-    api = ti.OpenApi(client)
-
-    markets = api.market.market_stocks_get()
-    df = dict()
-    k = 0
-    for MI in markets.instruments:
-        if (len(l) > 0 and MI.ticker in l) or (len(l) == 0 and MI.ticker):
-            if MI.currency=="USD":
-                now = datetime.now()
-                try:
-                    cndls = api.market.market_candles_get(MI.figi,
-                                                            from_=f,
-                                                            to=t,
-                                                            interval=ti.CandleResolution.day)
-                    
-                    df2 = dict()
-                    for cndl in cndls.candles:
-                        df2[str(cndl.time)] = cndl.c
-                    df[MI.ticker] = df2
-                except:
-                    pass
-                k = k+1
-    pddf = pd.DataFrame(df)
-    return pddf
-
 
 def getTinkoffETFsLYPrices(l=[], resolution='day'):
     client = CustomClient(
@@ -155,3 +90,27 @@ def getTinkoffETFsLYPrices(l=[], resolution='day'):
             k = k+1
     pddf = pd.DataFrame(df)
     return pddf
+
+def getActualPrices():
+    client = CustomClient(
+        os.getenv('TINVEST_SANDBOX_TOKEN', ''), use_sandbox=True)
+    api = ti.OpenApi(client)
+    markets = api.market.market_etfs_get()
+    dc = dict()
+    for MI in markets.instruments:
+        now = datetime.now()
+        try:
+            cndls = api.market.market_candles_get(MI.figi,
+                                                from_=now -
+                                                timedelta(days=7),
+                                                to=now,
+                                                interval=ti.CandleResolution.day)
+            df2 = dict()
+            cost = 0
+            for cndl in cndls.candles:
+                cost = cndl.c
+                dc[MI.ticker] = cost*MI.lot
+        except:
+            pass
+
+    return dc
